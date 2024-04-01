90d725ae8f6aea65d98199d0c34e0abe82bd5855
Ejenie <baranova.evg@inbox.ru>
2024-04-01 19:42:01 +0300

    Новая версия слежения

--- Ардуино/Find_fox_arduino/Find_fox_arduino.ino
+++ Ардуино/Find_fox_arduino/Find_fox_arduino.ino
@@ -1,12 +1,14 @@
 #include <Motors.h>
-
+#include "encoder.h"
+#include "interesting_functions.h"
 Motors motorA = Motors(9, 23, 22, 5, 48); //pinPWM, pinA, pinB, interruptNumber, directionPin
 
 bool direct = true;
-float kp = 1.5, kd = 0, u = 0;
-int i = 0, pos = 0, err_old = 0, vel = 40, err = 0;
+float kp = 1.5, kd = 0, ki = 0.001, u = 0, i = 0;
+int pos = 0, err_old = 0, vel = 40, err = 0;
 uint32_t myTimer = millis();
-int32_t value = 0;
+
+
 
 void setup() {
   Serial.begin(9600);
@@ -21,12 +23,15 @@ void loop() {
   if (Serial3.available() != 0) {
     pos = Serial3.read() - 80;
 
-    if ( (pos < 0 && pos > -5) || (pos > 0 && pos < 5))
+    if (-1 < pos && pos < 1)
       pos = 0;
-      
+
     if (pos != 0) {
       err = pos - (value / 5);
-      u = err * kp + (err - err_old) * kd;
+      i += err * ki;
+      if (abs(i) > 5)
+        i = 5 * sign(i);
+      u = err * kp + i + (err - err_old) * kd;
       err_old = err;
       if (u > 50)
         u = 50;
@@ -39,7 +44,7 @@ void loop() {
 
     Serial.print(pos);
     Serial.print("  ");
-    Serial.print(value/5);
+    Serial.print(value / 5);
     Serial.print("  ");
     Serial.println(u);
   }
--- Ардуино/Find_fox_arduino/encoder.h
+++ Ардуино/Find_fox_arduino/encoder.h
@@ -1,3 +1,5 @@
+int32_t value = 0;
+
 void enc() {
   if (digitalRead(48))
     value++;
