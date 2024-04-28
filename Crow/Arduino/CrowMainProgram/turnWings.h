92bc43bac763faef921ad01b79570db89e666f30
Ejenie <baranova.evg@inbox.ru>
2024-04-28 13:35:02 +0300

    Переделала структуру гита 4

--- Crow/Arduino/CrowMainProgram/turnWings.h
+++
@@ -1,6 +0,0 @@
-Servo wingTurnL;
-<<<<<<< HEAD:Crow/Arduino/CrowMainProgram/turnWings.h
-Servo wingTurnR;
-=======
-Servo wingTurnR;
->>>>>>> baccc084cccf172d7f3a836c15970256e20816eb:Ардуино/crow/crow/turnWings.h
